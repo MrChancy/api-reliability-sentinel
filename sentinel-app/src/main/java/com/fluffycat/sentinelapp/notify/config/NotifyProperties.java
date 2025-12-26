820fc29431cec842faa060df35d55d125f0924f8
MrChancy <csiq123@163.com>
2025-12-26 13:55:42 +0800

    补齐“告警策略配置”与默认值

--- sentinel-app/src/main/java/com/fluffycat/sentinelapp/notify/config/NotifyProperties.java
+++ sentinel-app/src/main/java/com/fluffycat/sentinelapp/notify/config/NotifyProperties.java
@@ -5,5 +5,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "sentinel.notify")
 public record NotifyProperties(
         String defaultTo,
-        String from
-) {}
+        String from,
+        Long resendIntervalSec,
+        Boolean ackResendEnabled,
+        ChannelEnabled channelEnabled
+) {
+    public record ChannelEnabled(Boolean email) {
+    }
+}
